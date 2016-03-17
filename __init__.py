14c0116e0761164d2a402883a3e5771f76c948f1
Samuel <samuel.linehan15@gmail.com>
2016-03-17 11:58:40 -0600

    started client-side

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
 
 db = MongoEngine(app)
+
+@app.route('/')
+def index():
+    return make_response(open('templates/index.html').read())
 
 if __name__ == '__main__':
     app.run()
